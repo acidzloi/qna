36e9ffc8f42733e6980104ca8d441105bdb52d36
root <root@localhost.localdomain>
2025-09-18 14:29:39 +0300

    Answers for part 7

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,10 +12,9 @@ Rails.application.configure do
   config.enable_reloading = false
 
   # Eager loading loads your entire application. When running a single test locally,
-  # this is usually not necessary, and can slow down your test suite. However, it's
-  # recommended that you enable it in continuous integration systems to ensure eager
-  # loading is working properly before deploying your code.
-  config.eager_load = ENV["CI"].present?
+  # this is usually not necessary, and can slow down your test suite. However, in
+  # this case we need it to prevent autoloading issues
+  config.eager_load = true
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{1.hour.to_i}" }
